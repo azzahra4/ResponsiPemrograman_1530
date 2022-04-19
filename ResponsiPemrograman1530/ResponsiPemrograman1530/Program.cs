8ff12fde9bc22024bd58734cd9acdcea6877cba3
azzahra4 <67765090+azzahra4@users.noreply.github.com>
2022-04-19 16:32:39 +0700

    Add files via upload

---
+++ ResponsiPemrograman1530/ResponsiPemrograman1530/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman1530
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklarasi Variabel
+            Karyawan karyawan = new Karyawan();
+            Console.WriteLine("Masukkan NIK Karyawan");
+            string Nik = Console.ReadLine();
+            karyawan.Nik = Nik;
+            Console.WriteLine("Masukkan Nama Karyawan");
+            string Nama = Console.ReadLine();
+            karyawan.Nama = Nama;
+            Console.WriteLine("Masukkan Gaji Karyawan");
+            int GajiBulanan = int.Parse(Console.ReadLine());
+            karyawan.GajiBulanan = GajiBulanan;
+
+
+            karyawan.informasiKaryawan();
+        }
+    }
+}
